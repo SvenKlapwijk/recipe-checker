679cd6505d67793e7ee76d340355da7bf703c635
Sven Klapwijk <svenklapwijk@gmail.com>
2024-02-27 02:38:55 +0100

    commit

--- react-basics-project-starter/src/App.jsx
+++ react-basics-project-starter/src/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
       {selectedRecipe ? (
         <RecipePage clickFn={setSelectedRecipe} item={selectedRecipe} />
       ) : (
-        <Text padding="0 1rem 0 1rem" minHeight="100vh" as="div" backgroundColor="blue.300">
+        <Text padding="0 1rem 0 1rem" width="100vw" minHeight="100vh" as="div" backgroundColor="blue.300">
           <Stack spacing="1.5rem">
             <Heading marginTop="1rem" textAlign="center" as="h1">
               Sven's recipe app
