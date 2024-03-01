d405de7f03fb12dc73e0765be54e8325fd28e2ab
SvenKlapwijk <159252863+SvenKlapwijk@users.noreply.github.com>
2024-03-01 13:31:51 +0100

    Removed console.log from RecipeListPage.jsx

--- react-basics-project-starter/src/pages/RecipeListPage.jsx
+++ react-basics-project-starter/src/pages/RecipeListPage.jsx
@@ -2,8 +2,6 @@ import { data } from "../utils/data";
 import { RecipeItemCard } from "../components/RecipeItemCard";
 
 export const RecipeListPage = ({ recipes, clickFn }) => {
-  console.log(recipes[0]);
-  console.log(data.hits[0]);
   return (
     <>
       {recipes.map((hit) => (
